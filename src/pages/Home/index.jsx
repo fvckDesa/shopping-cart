e22cfbc348222e4928b821feb036994542d20923
Luca De Santis <lucaroma2705@gmail.com>
2022-09-28 19:57:05 +0200

    create home page

---
+++ src/pages/Home/index.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import { IoIosArrowDropright } from "react-icons/io";
+
+import style from "./Home.module.css";
+
+function Home() {
+  return (
+    <div className={style.homePage}>
+      <div className={style.titleContainer}>
+        <h1 className={style.title}>
+          All Streetwear <br /> do you need
+        </h1>
+        <h2 className={style.subtitle}>
+          Hundreds of streetwear items for create your personal style
+        </h2>
+        <button type="button" className={style.shopBtn}>
+          <Link className={style.shopLink} to="/shop">
+            Shop now <IoIosArrowDropright />
+          </Link>
+        </button>
+      </div>
+      <img
+        className={style.illustration}
+        src="./shoes-illustration.png"
+        alt="shoes illustration"
+      />
+    </div>
+  );
+}
+
+export default Home;
