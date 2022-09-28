79d83bb875dd05fa8a7b0cfb1c40959969ca99c5
Luca De Santis <lucaroma2705@gmail.com>
2022-09-28 19:55:36 +0200

    configure app

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,20 @@
-import './App.css'
+// components
+import { Routes, Route } from "react-router-dom";
+import { Home, Shop, ShopItemPage, NotFound } from "@pages";
+import Navbar from "@components/Navbar";
 
 function App() {
   return (
     <div className="App">
-    
+      <Navbar />
+      <Routes>
+        <Route index element={<Home />} />
+        <Route path="shop" element={<Shop />} />
+        <Route path="shop/:id" element={<ShopItemPage />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/pages/index.js
@@ -0,0 +1,4 @@
+export { default as Home } from "./Home";
+export { default as Shop } from "./Shop";
+export { default as ShopItemPage } from "./ShopItemPage";
+export { default as NotFound } from "./NotFound";
