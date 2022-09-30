daa1a0dc2a8063b480923ba819609fc46e2063ed
Luca De Santis <lucaroma2705@gmail.com>
2022-09-30 14:15:24 +0200

    product page

--- src/pages/ShopItemPage/index.jsx
+++ src/pages/ShopItemPage/index.jsx
@@ -1,11 +1,58 @@
-import React from "react";
-import { useParams } from "react-router-dom";
-
+// hooks
+import { useState } from "react";
+import useAxios from "@hooks/useAxios";
+// components
+import { FaCartPlus } from "react-icons/fa";
+import { Ring } from "@uiball/loaders";
+// utils
+import { formatProductDescription } from "@src/utils";
+// style
 import style from "./ShopItemPage.module.css";
 
+const { VITE_RAPID_API_KEY: API_KEY } = import.meta.env;
+const baseURL = "/static/";
+
 function ShopItemPage() {
-  const { id } = useParams();
-  return <div>ShopItemPage {id}</div>;
+  const [isCompactDescription, setIsCompactDescription] = useState(true);
+  const { data, isLoading, error } = useAxios(baseURL + "productDetails.json", {
+    headers: {
+      "X-RapidAPI-Key": API_KEY,
+      "X-RapidAPI-Host": "klekt.p.rapidapi.com",
+    },
+  });
+  return (
+    <div className={style.shopItemPage}>
+      {isLoading && <Ring size={40} lineWeight={5} speed={2} color="black" />}
+      <div className={style.productInfo}>
+        <h2 className={style.productInfo__name}>{data?.name}</h2>
+        <p className={style.productInfo__description}>
+          {formatProductDescription(data?.description, isCompactDescription)}
+          <br />
+          <span onClick={() => setIsCompactDescription((prev) => !prev)}>
+            Read {isCompactDescription ? "more" : "less"}
+          </span>
+        </p>
+        <div className={style.actionsContainer}>
+          <button className={style.addToCartBtn} type="button">
+            <FaCartPlus /> Add to cart
+          </button>
+          <a
+            className={style.klektLink}
+            href={`https://www.klekt.com/product/${data?.slug}`}
+            target="_blanck"
+          >
+            Go on klekt
+            <div />
+          </a>
+        </div>
+      </div>
+      <img
+        className={style.productImage}
+        src={data?.assets[0].asset.preview}
+        alt="product image"
+      />
+    </div>
+  );
 }
 
 export default ShopItemPage;
--- src/utils/index.js
+++ src/utils/index.js
@@ -1,7 +1,17 @@
+const MAX_CHAR_COMPACT_DESCRIPTION = 80;
+
 export function parseShopData(data) {
   return data?.items.map(({ productId, productName, productAsset }) => ({
     id: productId,
     name: productName,
     image: productAsset.preview,
   }));
+}
+
+export function formatProductDescription(description, isCompact) {
+  if (!description) return "";
+  const validDescription = description.replace(/<(\/)?p>/g, "");
+  return isCompact
+    ? validDescription.slice(0, MAX_CHAR_COMPACT_DESCRIPTION) + "..."
+    : validDescription;
 }
